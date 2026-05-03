3c200e959d1c66070b515e400837cf453df262ee
Adithyan <aadithyan701@gmail.com>
2026-05-03 09:56:52 +0530

    deployment related

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://backend-cafe-belling-1.onrender.com',
+    adminUser: 'admin',
+    adminPassword: 'admin',
+  };
+  
+  export function adminAuthorizationHeader(): string {
+    return 'Basic ' + btoa(`${environment.adminUser}:${environment.adminPassword}`);
+  }
+  
